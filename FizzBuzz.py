b60719c7686ef81b109d9ec1a02a9a7fafbd4782
ffekirnew <ffekirnew0808@gmail.com>
2022-05-22 22:14:21 +0300

    First commit.

---
+++ FizzBuzz.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        list = []
+        for num in range(1, n + 1):
+            if (num % 3 == 0) and (n % 5 == 0):
+                list.append("FizzBuzz")
+            elif (num % 3 == 0):
+                list.append("Fizz")
+            elif num % 5 == 0:
+                list.append("Buzz")
+            else:
+                list.append(num)
+        
+        print(list)
