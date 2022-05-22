25857106eb66b1ed1159395abbf512e3c35daaf9
ffekirnew <ffekirnew0808@gmail.com>
2022-05-22 22:18:31 +0300

    changed n to num

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         list = []
         for num in range(1, n + 1):
-            if (num % 3 == 0) and (n % 5 == 0):
+            if (num % 3 == 0) and (num % 5 == 0):
                 list.append("FizzBuzz")
             elif (num % 3 == 0):
                 list.append("Fizz")
