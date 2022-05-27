68262ce3eba7e8cd6eae7e373be0ce871775e64a
ffekirnew <ffekirnew0808@gmail.com>
2022-05-27 09:14:03 +0300

    Solution to 'Power of Four'[Easy] problem from Leetcode's set

---
+++ Power of Four.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1 or n == 4:
+            return True
+        elif n % 4 != 0 or n < 4:
+            return False
+        else:
+            if n > 4:
+                return self.isPowerOfFour(n//4 + n % 4)
